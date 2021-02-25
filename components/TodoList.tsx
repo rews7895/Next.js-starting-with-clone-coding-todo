1ed4f7e6721cefd5faaed8db6c3a4ff635f6398a
Junseok <yalvz@naver.com>
2021-02-25 22:20:36 +0900

    todolist style 완성
    svg추가

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,6 +2,8 @@ import React, { useMemo, useCallback } from "react";
 import styled from "styled-components";
 import palette from "../styles/palette";
 import { TodoType } from "../types/todo";
+import TrashCanIcon from "../public/statics/svg/trash_can.svg";
+import CheckMarkIcon from "../public/statics/svg/check_mark.svg";
 
 const Container = styled.div`
   width: 100%;
@@ -85,7 +87,17 @@ const Container = styled.div`
       .todo-right-side {
         display: flex;
         margin-right: 12px;
-
+        svg {
+          &:first-child {
+            margin-right: 16px;
+          }
+        }
+        .todo-trash-can {
+          fill: ${palette.deep_red};
+        }
+        .todo-check-mark {
+          fill: ${palette.deep_green};
+        }
         .todo-button {
           width: 20px;
           height: 20px;
@@ -195,6 +207,12 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
               </p>
             </div>
             <div className="todo-right-side">
+              {todo.checked && (
+                <>
+                  <TrashCanIcon className="todo-trash-can" onClick={() => {}} />
+                  <CheckMarkIcon className="todo-check-mark" onClick={() => {}} />
+                </>
+              )}
               {!todo.checked && (
                 <button type="button" className="todo-button" onClick={() => {}} />
               )}
